f76c9a2766e07037774b617f92c259e67dba95dd
Thaddeus Krugman <thaddeuskrugman@gmail.com>
2023-09-12 22:12:35 -0400

    added try catch to api calls

--- src/routes/property.ts
+++ src/routes/property.ts
@@ -19,56 +19,29 @@ property.get('/', async (c) => {
 
 // Get a single property
 property.get('/:id', async (c) => {
-  const id = Number(c.req.param('id'));
-  const property = await prisma.property.findUnique({
-    where: { id },
-  });
+  try {
+    const id = Number(c.req.param('id'));
+    const property = await prisma.property.findUnique({
+      where: { id },
+    });
 
-  if (property === null) {
-    return c.notFound();
+    if (property === null) {
+      return c.notFound();
+    }
+
+    return c.json(property);
+  } catch (error) {
+    console.error('Error fetching property:', error);
+    return c.text('Internal Server Error', 500);
   }
-
-  return c.json(property);
 });
 
 // Create a property
 property.post('/', async (c) => {
-  const data = await c.req.json();
+  try {
+    const data = await c.req.json();
 
-  const {
-    createdBy,
-    imageUrls,
-    title,
-    description,
-    price,
-    numberOfBeds,
-    numberOfBaths,
-    sqft,
-    propertyType,
-    isFeatured,
-    isActive,
-    isSold,
-  } = data;
-
-  if (
-    !createdBy ||
-    !imageUrls ||
-    !title ||
-    !description ||
-    price === undefined ||
-    numberOfBeds === undefined ||
-    numberOfBaths === undefined ||
-    !sqft === undefined ||
-    !propertyType ||
-    isFeatured === undefined ||
-    isActive === undefined ||
-    isSold === undefined
-  ) {
-    return c.text('All fields must be provided', 400);
-  }
-
-  const property = await prisma.property.create({
-    data: {
+    const {
       createdBy,
       imageUrls,
       title,
@@ -81,52 +54,56 @@ property.post('/', async (c) => {
       isFeatured,
       isActive,
       isSold,
-    },
-  });
+    } = data;
 
-  return c.json(property);
+    if (
+      !createdBy ||
+      !imageUrls ||
+      !title ||
+      !description ||
+      price === undefined ||
+      numberOfBeds === undefined ||
+      numberOfBaths === undefined ||
+      !sqft === undefined ||
+      !propertyType ||
+      isFeatured === undefined ||
+      isActive === undefined ||
+      isSold === undefined
+    ) {
+      return c.text('All fields must be provided', 400);
+    }
+
+    const property = await prisma.property.create({
+      data: {
+        createdBy,
+        imageUrls,
+        title,
+        description,
+        price,
+        numberOfBeds,
+        numberOfBaths,
+        sqft,
+        propertyType,
+        isFeatured,
+        isActive,
+        isSold,
+      },
+    });
+
+    return c.json(property);
+  } catch (error) {
+    console.error('Error creating property:', error);
+    return c.text('Internal Server Error', 500);
+  }
 });
 
 // Update a property by ID
 property.put('/:id', async (c) => {
-  const id = Number(c.req.param('id'));
-  const data = await c.req.json();
+  try {
+    const id = Number(c.req.param('id'));
+    const data = await c.req.json();
 
-  const {
-    createdBy,
-    imageUrls,
-    title,
-    description,
-    price,
-    numberOfBeds,
-    numberOfBaths,
-    sqft,
-    propertyType,
-    isFeatured,
-    isActive,
-    isSold,
-  } = data;
-
-  if (
-    !createdBy ||
-    !imageUrls ||
-    !title ||
-    !description ||
-    price === undefined ||
-    numberOfBeds === undefined ||
-    numberOfBaths === undefined ||
-    !sqft ||
-    !propertyType ||
-    isFeatured === undefined ||
-    isActive === undefined ||
-    isSold === undefined
-  ) {
-    return c.text('All fields must be provided', 400);
-  }
-
-  const property = await prisma.property.update({
-    where: { id },
-    data: {
+    const {
       createdBy,
       imageUrls,
       title,
@@ -139,30 +116,73 @@ property.put('/:id', async (c) => {
       isFeatured,
       isActive,
       isSold,
-    },
-  });
+    } = data;
 
-  return c.json(property);
+    if (
+      !createdBy ||
+      !imageUrls ||
+      !title ||
+      !description ||
+      price === undefined ||
+      numberOfBeds === undefined ||
+      numberOfBaths === undefined ||
+      !sqft ||
+      !propertyType ||
+      isFeatured === undefined ||
+      isActive === undefined ||
+      isSold === undefined
+    ) {
+      return c.text('All fields must be provided', 400);
+    }
+
+    const property = await prisma.property.update({
+      where: { id },
+      data: {
+        createdBy,
+        imageUrls,
+        title,
+        description,
+        price,
+        numberOfBeds,
+        numberOfBaths,
+        sqft,
+        propertyType,
+        isFeatured,
+        isActive,
+        isSold,
+      },
+    });
+
+    return c.json(property);
+  } catch (error) {
+    console.error('Error updating property:', error);
+    return c.text('Internal Server Error', 500);
+  }
 });
 
 // Delete a property by ID
 property.delete('/:id', async (c) => {
-  const id = Number(c.req.param('id'));
+  try {
+    const id = Number(c.req.param('id'));
 
-  // Check if property exists before deleting
-  const existingProperty = await prisma.property.findUnique({
-    where: { id },
-  });
+    // Check if property exists before deleting
+    const existingProperty = await prisma.property.findUnique({
+      where: { id },
+    });
 
-  if (existingProperty === null) {
-    return c.notFound();
+    if (existingProperty === null) {
+      return c.notFound();
+    }
+
+    await prisma.property.delete({
+      where: { id },
+    });
+
+    return c.text('Property deleted successfully');
+  } catch (error) {
+    console.error('Error deleting property:', error);
+    return c.text('Internal Server Error', 500);
   }
-
-  await prisma.property.delete({
-    where: { id },
-  });
-
-  return c.text('Property deleted successfully');
 });
 
 export default property;
